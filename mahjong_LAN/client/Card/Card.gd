78d75616d1d1f7a7b535300086eecc4001849a4c
liweimin <2410250284@qq.com>
2022-01-11 20:57:48 +0800

    调整文件目录位置

---
+++ mahjong_LAN/client/Card/Card.gd
@@ -0,0 +1,20 @@
+extends TextureRect
+
+enum CardPlayer {
+	left,
+	down,
+	up,
+	right
+}
+enum CardState {
+	zhanzhe,tangzhe,pazhe,dachu
+}
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func change_card_state():
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
