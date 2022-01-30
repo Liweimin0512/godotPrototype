c1446faedcc53d2f9a6bdd009143c9cd868b372c
DESKTOP-LU6ELKI\24102 <2410250284@qq.com>
2022-01-30 18:34:30 +0800

    仿照完成一个异步加载csv文件的方式

--- game_demo_gd/survival/addons/QFramework/datatable/csv_datatable_manager.gd
+++ game_demo_gd/survival/addons/QFramework/datatable/csv_datatable_manager.gd
@@ -1,6 +1,8 @@
 extends DatatableManager
 class_name CSVDatatableManager
 
+var thread := Thread.new()
+
 func get_datatable(datatable_name : String) -> Dictionary:
 	if _datatable_dics.has(datatable_name):
 		return _datatable_dics[datatable_name]
@@ -8,6 +10,17 @@ func get_datatable(datatable_name : String) -> Dictionary:
 		load_datatable(datatable_name, _datatable_path)
 		return _datatable_dics[datatable_name]		
 
+func load_datatables_async(datatables : Array) -> Array:
+	var datatables_in = datatables.duplicate()
+	var out := []
+	if can_async:
+		thread.start(self, 'threaded_load', datatables_in)
+		out = yield(self, "done")
+		thread.wait_to_finish()
+	else:
+		print("can not async load csv")
+	return out
+	
 func load_datatable(name : String, path : String) :
 	var file = File.new()
 	file.open(path + name + ".csv", File.READ)
@@ -22,3 +35,21 @@ func load_datatable(name : String, path : String) :
 			d[data_name[i]] = row_data[i]
 		dic_dt[d["ID"]] = d
 	_datatable_dics[name] = dic_dt
+
+func threaded_load(datatables_in : Array) -> void:
+	var datatables_out = []
+	var file = File.new()
+	for dat_in in datatables_in:
+		file.open(dat_in, File.READ)
+		var row_data = file.get_csv_line(",")
+		var data_name = row_data
+		
+		var dic_dt = {}
+		while not file.eof_reached():
+			row_data = file.get_csv_line(",")
+			var d = {}
+			for i in row_data.size():
+				d[data_name[i]] = row_data[i]
+			dic_dt[d["ID"]] = d
+		datatables_out.append(dic_dt)
+	call_deferred('emit_signal', 'done', datatables_out)
--- game_demo_gd/survival/addons/QFramework/datatable/manager_datatable.gd
+++ game_demo_gd/survival/addons/QFramework/datatable/manager_datatable.gd
@@ -7,6 +7,8 @@ signal load_datatable_fail
 var _datatable_dics : Dictionary = {}
 export var _datatable_path : String = "res://datatables"
 
+var can_async : bool = ["Windows","OSX","UWP", "X11"].has(OS.get_name())
+
 func get_datatable(datatable_name : String) -> Dictionary:
 	return {}
 
@@ -16,6 +18,3 @@ func load_datatable(name : String, path : String) :
 func set_datatable_path(path:String) -> void:
 	_datatable_path = path
 
-#TODO 异步加载数据表
-func load_datatable_asyn(name : String, path : String) :
-	pass
